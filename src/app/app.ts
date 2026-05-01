ade12bc05bffa9697bc97e9df8006607bb5af3f1
Pawelv9 <Pawelv9@gmail.com>
2026-05-01 23:22:26 +0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('investment-calculator');
+}
