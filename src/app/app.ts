497ccb9264860c7b6eeafa06b76f1fffb35a8b1e
Pawelv9 <Pawelv9@gmail.com>
2026-05-04 23:19:01 +0300

    adding a User Input Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    Header
+    Header,
+    UserInput
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ src/app/user-input/user-input.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-input.html',
+  styleUrl: './user-input.css',
+})
+export class UserInput {}
